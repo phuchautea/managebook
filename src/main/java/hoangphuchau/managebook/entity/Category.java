4222e743650c62a9def2dd71cbe0901d47d579f1
phuchau.tea <phuchau.tea@gmail.com>
2023-05-24 13:56:12 +0700

    fix: validate name of category

--- src/main/java/hoangphuchau/managebook/entity/Category.java
+++ src/main/java/hoangphuchau/managebook/entity/Category.java
@@ -1,6 +1,8 @@
 package hoangphuchau.managebook.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -11,7 +13,9 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name")
+    @Column(name = "name", length = 50, nullable = false)
+    @Size(max = 50, message = "Your name must be less than 50 characters")
+    @NotBlank(message = "Your name is required")
     private String name;
     @OneToMany (mappedBy = "category", cascade = CascadeType.ALL)
     private List<Book> books;
